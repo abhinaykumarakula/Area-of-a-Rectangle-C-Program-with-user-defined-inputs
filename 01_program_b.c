2fa619ce857b2619b292f5367d16e228d3cd5d92
ABHINAY <abhinaykumarakula5@gmail.com>
2025-04-29 23:29:33 +0530

    Add files via upload

---
+++ 01_program_b.c
@@ -0,0 +1,25 @@
+// OM NAMO VENKATESHAYA
+// AUTHOR     : ABHINAY
+/* PROGRAM    : AREA OF RECTANGLE BY USING
+                USER DEFINED INPUTS */
+ 
+     #include <stdio.h> 
+ 
+         int main(){ 
+            int length,breadth,area;
+       
+            printf("enter the length :\n");
+            scanf("%d",&length);
+
+            printf("enter the breadth :\n");
+            scanf("%d",&breadth);
+
+            //area of rectangle = length*breadth
+            
+              area = length*breadth;
+
+            
+            printf("AREA OF THE RECTANGLE IS :%d\n",area);
+            return 0;
+ }
+
